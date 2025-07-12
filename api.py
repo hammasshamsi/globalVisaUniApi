9b30eef8670d8eceac9d4ce1d091a40d8b1675ac
Hammas Shamsi <100429348+hammasshamsi@users.noreply.github.com>
2025-07-12 15:55:10 +0500

    Update api.py

--- api.py
+++ api.py
@@ -77,4 +77,6 @@ def recommend():
         return jsonify({'status': 'error', 'message': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
